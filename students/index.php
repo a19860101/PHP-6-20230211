b546b784b713dcc97b6f99774eb609419a01ceee
Zac <a19860101@gmail.com>
2023-02-25 11:52:41 +0800

    20230225

---
+++ students/index.php
@@ -0,0 +1,18 @@
+<?php
+    include('db.php');
+    $sql = 'SELECT * FROM students';
+    $students = $pdo->query($sql)->fetchAll();
+    var_dump($students);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
