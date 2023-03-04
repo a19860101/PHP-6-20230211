96d4073d1637d428b0c592197eff297d30eae4d4
Zac <a19860101@gmail.com>
2023-03-04 15:40:15 +0800

    20230304

--- member/function.php
+++ member/function.php
@@ -8,7 +8,15 @@
             $stmt = pdo()->prepare($sql);
             $pw = password_hash($pw,PASSWORD_DEFAULT);
             $stmt->execute([$name,$email,$pw,now()]);
+            return [
+                'errCode' => 0,
+                'status' => '註冊成功'
+            ];
         }catch(PDOException $e){
             echo $e->getMessage();
+            return [
+                'errCode' => 1,
+                'status' => '註冊失敗'
+            ];
         }
     }
--- member/store.php
+++ member/store.php
@@ -1,6 +1,11 @@
 <?php
     include('function.php');
     $result = store($_REQUEST);
-
-    echo '<script>alert("註冊成功，請重新登入");</script>';
-    header('refresh:0;url=index.php');
+    extract($result);
+    if($errCode == 0){
+        echo '<script>alert("'.$status.'");</script>';
+        header('refresh:0;url=index.php');
+    }else{
+        echo '<script>alert("'.$status.'");</script>';
+        header('refresh:0;url=register.php');
+    }
