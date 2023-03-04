7a4369034979c0acbdf5e47674913f6238256ad9
Zac <a19860101@gmail.com>
2023-03-04 15:24:01 +0800

    20230304

--- member/function.php
+++ member/function.php
@@ -1,6 +1,14 @@
 <?php
     include('db.php');
 
-    function store(){
-        
+    function store($request){
+        extract($request);
+        $sql = 'INSERT INTO users(name,email,pw,created_at)VALUES(?,?,?,?)';
+        try{
+            $stmt = pdo()->prepare($sql);
+            $pw = password_hash($pw,PASSWORD_DEFAULT);
+            $stmt->execute([$name,$email,$pw,now()]);
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
     }
