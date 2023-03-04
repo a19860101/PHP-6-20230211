d7ee428765b0267a130ad053d8e340f5f5317a83
Zac <a19860101@gmail.com>
2023-03-04 15:06:03 +0800

    20230304

--- member/db.php
+++ member/db.php
@@ -1,21 +1,29 @@
 <?php
-function pdo(){
-    $db_host = 'localhost';
-    $db_user = 'admin';
-    $db_pw = 'admin';
-    $db_name = 'php-6-20230211';
-    $db_charset = 'utf8mb4';
-    
-    // data source name
-    $dsn = "mysql:host={$db_host};dbname={$db_name};charset={$db_charset}";
-    
-    try{   
-        //pdo物件
-        $pdo = new PDO($dsn,$db_user,$db_pw);
-    }catch(PDOException $e){
-        // var_dump($e);
-        echo $e->getMessage();
+    function pdo(){
+        $db_host = 'localhost';
+        $db_user = 'admin';
+        $db_pw = 'admin';
+        $db_name = 'php-6-20230211';
+        $db_charset = 'utf8mb4';
+        
+        // data source name
+        $dsn = "mysql:host={$db_host};dbname={$db_name};charset={$db_charset}";
+        
+        try{   
+            //pdo物件
+            $pdo = new PDO($dsn,$db_user,$db_pw);
+        }catch(PDOException $e){
+            // var_dump($e);
+            echo $e->getMessage();
+        }
+        return $pdo;
     }
-    return $pdo;
-}
+    function now(){
+        // 設定時區
+        date_default_timezone_set('Asia/Taipei');
+        // 設定當下時間
+        $now = date('Y-m-d H:i:s');
+
+        return $now;
+    }
 
---
+++ member/register.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <div>
+            <label for="">姓名</label>
+            <input type="text" name="name">
+        </div>
+        <div>
+            <label for="">Email</label>
+            <input type="text" name="email">
+        </div>
+        <div>
+            <label for="">密碼</label>
+            <input type="text" name="pw">
+        </div>
+        <input type="submit" value="註冊">
+        <input type="button" value="取消" onclick="history.back()">
+    </form>
+</body>
+</html>
