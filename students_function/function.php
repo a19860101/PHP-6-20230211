3b710c6eab940ce87e1e8ba87344fc192c09e21c
Zac <a19860101@gmail.com>
2023-03-04 12:00:49 +0800

    20230304

--- students_function/function.php
+++ students_function/function.php
@@ -2,7 +2,7 @@
     include('db.php');
     function index(){
         // $pdo = pdo();
-        $sql = 'SELECT * FROM students1 ORDER BY id DESC';
+        $sql = 'SELECT * FROM students ORDER BY id DESC';
         // $students = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
         try{
             $students = pdo()->query($sql)->fetchAll(PDO::FETCH_ASSOC);
@@ -21,35 +21,90 @@
         $sql = 'SELECT * FROM students WHERE id = ?';
         $stmt = pdo()->prepare($sql);
         $stmt->execute([$id]);
-        $student = $stmt->fetch(PDO::FETCH_ASSOC);
-        return $student;
+        try{
+            $student = $stmt->fetch(PDO::FETCH_ASSOC);
+            return $student;
+        }catch(PDOException $e){
+            return [
+                'errCode'=>$e->getCode(),
+                'errMsg'=>$e->getMessage()
+            ];
+        }
     }
     function store($request){
         extract($request);
         $sql = 'INSERT INTO students(name,phone,email,edu,gender,skill)VALUES(?,?,?,?,?,?)';
         $stmt = pdo()->prepare($sql);
-        $skill = implode(',',$skill);
-        $stmt->execute([$name,$phone,$email,$edu,$gender,$skill]);
+        if($name == ''){
+            return [
+                'err' => 1,
+                'status' => '請填寫姓名'
+            ];
+        }
+        if($phone == ''){
+            return [
+                'err' => 1,
+                'status' => '請填寫電話'
+            ];
+        }
+        if(!isset($skill) || !isset($gender)){
+            return [
+                'err' => 1,
+                'status' => '請填寫所有資料'
+            ];
+        }else{
+            $skill = implode(',',$skill);
+        }
+
+        try{   
+            $stmt->execute([$name,$phone,$email,$edu,$gender,$skill]);
+        }catch(PDOException $e){
+            return [
+                'errCode'=>$e->getCode(),
+                'errMsg'=>$e->getMessage()
+            ];
+        }
     }
     function delete($request){
         extract($request);
         $sql = 'DELETE FROM students WHERE id = ?';
         $stmt = pdo()->prepare($sql);
-        $stmt->execute([$id]);
+        try{
+            $stmt->execute([$id]);
+        }catch(PDOException $e){
+            return [
+                'errCode'=>$e->getCode(),
+                'errMsg'=>$e->getMessage()
+            ];
+        }
     }
     function edit($request){
         extract($request);
         $sql = 'SELECT * FROM students WHERE id = ?';
         $stmt = pdo()->prepare($sql);
         $stmt->execute([$id]);
-        $student = $stmt->fetch(PDO::FETCH_ASSOC);
-        return $student;
+        try{
+            $student = $stmt->fetch(PDO::FETCH_ASSOC);
+            return $student;
+        }catch(PDOException $e){
+            return [
+                'errCode'=>$e->getCode(),
+                'errMsg'=>$e->getMessage()
+            ];
+        }
     }
     function update($request){
         extract($request);
         $sql = 'UPDATE students SET name=?,email=?,phone=?,edu=?,gender=?,skill=? WHERE id =?';
         $stmt = pdo()->prepare($sql);
         $skill = implode(',',$skill);
-        $stmt->execute([$name,$email,$phone,$edu,$gender,$skill,$id]);
+        try{
+            $stmt->execute([$name,$email,$phone,$edu,$gender,$skill,$id]);
+        }catch(PDOException $e){
+            return [
+                'errCode'=>$e->getCode(),
+                'errMsg'=>$e->getMessage()
+            ];
+        }
 
     }
--- students_function/store.php
+++ students_function/store.php
@@ -1,5 +1,15 @@
 <?php
     include('function.php');
-    store($_REQUEST);
-    echo '<script>alert("資料已新增");</script>';
-    header('refresh:0;url=index.php');
+    $result = store($_REQUEST);
+    
+    if(isset($result['errCode'])){
+        echo $result['errMsg'];
+        return;
+    }
+    if(!isset($result['err'])){
+        echo '<script>alert("資料已新增");</script>';
+        header('refresh:0;url=index.php');
+    }else{
+        echo '<script>alert("'.$result['status'].'");</script>';
+        header('refresh:0;url=create.php');
+    }
