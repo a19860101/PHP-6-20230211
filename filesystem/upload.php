c643940382cc2f2708a9a1ec998c1974eb674000
Zac <a19860101@gmail.com>
2023-03-11 10:54:25 +0800

    20230311

--- filesystem/upload.php
+++ filesystem/upload.php
@@ -2,5 +2,15 @@
     // var_dump($_REQUEST);
     // var_dump($_FILES['img']);
     extract($_FILES['img']);
-    echo $name;
-    echo $size;
+
+    $target = 'images/'.$name;
+
+    if($error == 0){
+        if(move_uploaded_file($tmp_name,$target)){
+            echo '上傳失敗';
+        }
+    }else{
+        echo '上傳錯誤';
+    }
+
+    header('location:index.php');
