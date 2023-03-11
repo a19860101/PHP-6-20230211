ddc8e3b6f8865cd64340511a323b42781d455d7a
Zac <a19860101@gmail.com>
2023-03-11 14:20:54 +0800

    20230311

--- filesystem/index.php
+++ filesystem/index.php
@@ -15,9 +15,18 @@
         <?php
             $imgs = glob('images/*');
             // var_dump($imgs);
-            foreach($imgs as $img){
+            $img_nums = count($imgs);
+            
         ?>
-        <img src="<?php echo $img;?>" alt="" width="150">
+        <div><?php echo "目前共{$img_nums}張圖";?> </div>
+        <?php  foreach($imgs as $img){ ?>
+        <div>
+            <img src="<?php echo $img;?>" alt="" width="150">
+            <form action="" method="post">
+                <input type="hidden" name="img" value="<?php echo $img;?>">
+                <input type="submit" value="刪除圖片" onclick="return confirm('確認刪除?')">
+            </form>
+        </div>
         <?php } ?>
     </div>
 </body>
