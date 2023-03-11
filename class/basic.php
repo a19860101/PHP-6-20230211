954c2a4e8f8a6e71c01fc171266da9fa3abc6185
Zac <a19860101@gmail.com>
2023-03-11 16:21:26 +0800

    20230311

--- class/basic.php
+++ class/basic.php
@@ -3,15 +3,26 @@
         // 屬性
         public $name = 'John Doe';
         public $gender = 'Male';
+
+        //方法
+        public function walk(){
+            return $this->name.' walk!!';
+        }
+        public function jump(){
+            return $this->name.' jump!';
+        }
     }
 
     $user_1 = new User;
 
     // var_dump($user_1);
 
-    echo $user_1 -> name;
-    echo $user_1 -> gender;
+    // echo $user_1 -> name;
+    // echo $user_1 -> gender;
+    $user_1->name = 'Max';
+    echo $user_1 -> jump();
 
     $user_2 = new User;
     $user_2 -> name = 'Mary';
-    echo $user_2 -> name;
+    // echo $user_2 -> name;
+    echo $user_2 -> walk();
