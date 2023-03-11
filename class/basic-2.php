793b557c49a6dc799fddf8d0c3c2555f7ac419fe
Zac <a19860101@gmail.com>
2023-03-11 16:56:00 +0800

    20230311

--- class/basic-2.php
+++ class/basic-2.php
@@ -1,6 +1,9 @@
 <?php
     class User {
         public $name = 'John Doe';
+        public function __construct(){
+            echo '建構子';
+        }
         public function walk(){
             return $this->name.' walk';
         }
@@ -18,9 +21,15 @@
     // echo User::attack();
 
     
-    // $admin = new Admin;
+    $admin = new Admin;
     // $admin->name = 'Administrator';
     // echo $admin->walk();
     // echo $admin->attack();
-    
-    echo Admin::attack();
+
+    // echo Admin::attack();
+
+    $user_1 = new User;
+    $user_2 = new User;
+    $admin_1 = new Admin;
+
+    echo User::attack();
