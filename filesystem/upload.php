68d2ffd92917b8626bfe4098c4918dd7ce6e412e
Zac <a19860101@gmail.com>
2023-03-11 12:06:21 +0800

    20230311

--- filesystem/upload.php
+++ filesystem/upload.php
@@ -15,13 +15,26 @@
 
     $target = $folder.$fullname;
 
+    switch($error){
+        case 0:
+            if(move_uploaded_file($tmp_name,$target)){
+                echo '<script>alert("檔案已上傳")</script>';
+            }else{
+                echo '上傳失敗';
+            }
+            break;
+        case 4:
+            echo '<script>alert("請選擇檔案")</script>';
+            break;
 
-    if($error == 0){
-        if(move_uploaded_file($tmp_name,$target)){
-            echo '上傳失敗';
-        }
-    }else{
-        echo '上傳錯誤';
     }
-    echo '<script>alert("檔案已上傳")</script>';
+
+    // if($error == 0){
+    //     if(move_uploaded_file($tmp_name,$target)){
+    //         echo '上傳失敗';
+    //     }
+    // }else{
+    //     echo '上傳錯誤';
+    // }
+    
     header('refresh:1;url=index.php');
