1b02208798709797ac7c96e5ad82bde42905c47f
Zac <a19860101@gmail.com>
2023-03-18 11:12:21 +0800

    20230318

--- web/public/index.php
+++ web/public/index.php
@@ -1,19 +1,4 @@
 <?php
-    include('../vendor/autoload.php');
-    use Gjun\Web\Config\DB;
-    // $db = new Gjun\Web\Config\DB;
-    $db = new DB;
-    echo $db->now();
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>composer</h1>
-</body>
-</html>
+    include('template/header.php');
+    include('template/nav.php');
+    include('template/footer.php');
