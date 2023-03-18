696e360bde052ae197d5d37084f34db999499382
Zac <a19860101@gmail.com>
2023-03-18 14:32:23 +0800

    20230318

--- web/public/post/index.php
+++ web/public/post/index.php
@@ -1,21 +1,28 @@
+<?php
+    include('../../vendor/autoload.php');
+    use Gjun\Web\Controller\Post;
+    $posts = Post::index();
+?>
 <?php include('../template/header.php'); ?>
 <?php include('../template/nav.php'); ?>
 <div class="container">
     <div class="row">
-        <div class="col-12">
+        <?php foreach($posts as $post){ ?>
+        <div class="col-12 py-3">
             <div class="border p-4 rounded">
-                <small class="text-muted">科技</small>
-                <h3 class="mb-1">title</h3>
+                <small class="text-muted"><?php echo $post['category_id']; ?></small>
+                <h3 class="mb-1"><?php echo $post['title']; ?></h3>
                 <div class="my-4">
-                    一方面地理孤獨不得不傢俱傢俱多媒體加快精英古代就是設為灌水，最高而已地下我很市場價還能授權最後，不必人數帶來供應商要去為何，另外獲得平均報名不到案件方向千萬北方校園地區，女友下載青春處罰隱藏版不行聯繫電話先進教育檢驗才能法律責任，更好中學認真，夥伴有了豪。
+                    <?php echo $post['body']; ?>
                 </div>
                 <div class="mb-3">
-                    <small class="text-muted">2023-03-18 14:05:23</small>
+                    <small class="text-muted"><?php echo $post['created_at']; ?></small>
                 </div>
 
                 <a href="#" class="btn btn-primary">繼續閱讀</a>
             </div>
         </div>
+        <?php } ?>
     </div>
 </div>
 <?php include('../template/footer.php'); ?>
--- web/src/Controller/Post.php
+++ web/src/Controller/Post.php
@@ -3,6 +3,11 @@
     // use PDO;
     use Gjun\Web\Config\DB;
     class Post {
+        static function index(){
+            $sql = 'SELECT * FROM posts ORDER BY id DESC';
+            $data = DB::pdo()->query($sql)->fetchAll(\PDO::FETCH_ASSOC);
+            return $data;
+        }
         static function store($request){
             extract($request);
             $sql = 'INSERT INTO posts(title,body,category_id,created_at)VALUES(?,?,?,?)';
