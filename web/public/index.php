555a2321f993935c0d16dcab8d770e6431f498ae
Zac <a19860101@gmail.com>
2023-04-01 16:01:54 +0800

    20230401

--- web/public/index.php
+++ web/public/index.php
@@ -14,11 +14,11 @@
             <h2>作品展示</h2>
         </div>
         <?php foreach($galleries as $g){ ?>
-        <div class="col-lg-3 col-6">
-            <figure>
-                <img src="gallery/images/<?php echo $g['img_name'];?>" alt="" class="w-100 h-100">
-                <figcaption><?php echo $g['title'];?></figcaption>
+        <div class="col-lg-3 col-6 pb-3">
+            <figure class="ratio ratio-1x1">
+                <img src="gallery/images/<?php echo $g['img_name'];?>" alt="" class="w-100 h-100 object-fit-cover">
             </figure>
+            <div><?php echo $g['title'];?></div>
         </div>
         <?php } ?>
     </div>
@@ -28,7 +28,7 @@
         </div>
         <?php foreach($posts as $post){ ?>
         <div class="col-lg-3 col-6 py-3">
-            <div class="border p-4 rounded">
+            <div class="border p-4 rounded h-100">
                 <small class="text-muted"><?php echo $post['category_title']; ?></small>
                 <h3><?php echo $post['title']; ?></h3>
                 <small class="text-muted">
@@ -36,13 +36,21 @@
                     [<?php echo $post['email']; ?>]
                 </small>
                 <div class="my-4">
-                    <?php echo $post['body']; ?>
+                    <?php
+                        // $body = mb_substr($post['body'],0,30);
+                        // echo $post['body']; 
+                        // echo $body;
+                        echo mb_substr($post['body'],0,30);
+                    ?>
                 </div>
                 <div class="mb-3">
-                    <small class="text-muted"><?php echo $post['created_at']; ?></small>
+                    <small class="text-muted">
+                        <?php echo $post['created_at']; ?>
+                    
+                    </small>
                 </div>
 
-                <a href="show.php?id=<?php echo $post['id'];?>" class="btn btn-primary">繼續閱讀</a>
+                <a href="post/show.php?id=<?php echo $post['id'];?>" class="btn btn-primary">繼續閱讀</a>
             </div>
         </div>
         <?php } ?>
