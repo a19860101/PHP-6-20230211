041cc58a9b3ced30df78d1987e8e8816a0849315
Zac <a19860101@gmail.com>
2023-04-29 11:43:16 +0800

    20230429

---
+++ laravel/database/migrations/2023_04_29_113808_create_tags_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->timestamps();
+        });
+        Schema::create('post_tag',function(Blueprint $table){
+            $table->id();
+            $table->foreignId('post_id')->nullable()->constrained();
+            $table->foreignId('tag_id')->nullable()->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('post_tag',function(Blueprint $table){
+            $table->dropForeign(['tag_id']);
+            $table->dropForeign(['post_id']);
+        });
+        Schema::dropIfExists('tags');
+        Schema::dropIfExists('post_tag');
+    }
+};
