17be93e2e2ca98de0a024a1cb7014198153aed3e
Zac <a19860101@gmail.com>
2023-04-29 14:49:42 +0800

    20230429

--- laravel/resources/views/article/index.blade.php
+++ laravel/resources/views/article/index.blade.php
@@ -6,6 +6,14 @@
         <img src="/images/{{$article->cover}}" alt="" width="200">
         <h2>{{$article->title}}</h2>
         <div>作者:{{$article->user->name}}</div>
+        @php
+            $tagArray = [];
+            foreach($article->tags as $tag){
+                $tagArray[] = $tag->title;
+            }
+            $tagStr = implode(',',$tagArray);
+        @endphp
+        <div>標籤:{{$tagStr}}</div>
         <div>分類:{{$article->category->title}}</div>
         <small>建立時間{{$article->created_at}}</small>
         <div>
