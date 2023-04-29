32213f6bc03b74f0757b9fa9f393c03174779ae9
Zac <a19860101@gmail.com>
2023-04-29 15:35:35 +0800

    20230429

--- laravel/resources/views/template/master.blade.php
+++ laravel/resources/views/template/master.blade.php
@@ -9,10 +9,37 @@
 </head>
 <body>
     <nav>
-        <a href="/article">文章列表</a>
-        <a href="/article/create">新增文章</a>
-        <a href="/category">分類管理</a>
+        <div>
+            <a href="/article">文章列表</a>
+            <a href="/article/create">新增文章</a>
+            @auth
+            <a href="/category">分類管理</a>
+            @endauth
+        </div>
+        <div>
+            @auth
+            <a href="{{route('dashboard')}}">Dashboard</a>
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+                <input type="submit" value="logout">
+            </form>
+
+            @else
+
+            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+            @if (Route::has('register'))
+                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+            @endif
+
+            @endauth
+        </div>
     </nav>
+    <div>
+        @auth
+        會員你好
+        @endauth
+    </div>
     @yield('main')
 </body>
 </html>
